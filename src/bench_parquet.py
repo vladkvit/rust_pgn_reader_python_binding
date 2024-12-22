1ad13fa8a8be837214fea19f62f8dbd254ee464f
vladkvit <vladkvit@outlook.com>
2024-12-22 13:22:24 -0500

    Make benchmarks not disk-bound; add some more examples

--- src/bench_parquet.py
+++ src/bench_parquet.py
@@ -7,16 +7,15 @@ from datetime import datetime
 file_path = "2013-07-train-00000-of-00001.parquet"
 
 
+pf = pq.ParquetFile(file_path)
+pylist = pf.read(columns=["movetext"]).column("movetext").to_pylist()
+
 a = datetime.now()
 
-pf = pq.ParquetFile(file_path)
-
-for i in range(pf.num_row_groups):
-    table = pf.read_row_group(0, columns=["movetext"]).column("movetext").to_pylist()
-    for row in table:
-        extractor = rust_pgn_reader_python_binding.parse_game(row)
-        moves = extractor.moves
-        comments = extractor.comments
+for row in pylist:
+    extractor = rust_pgn_reader_python_binding.parse_game(row)
+    moves = extractor.moves
+    comments = extractor.comments
 
 b = datetime.now()
 print(b - a)
--- src/bench_parquet_parallel.py
+++ src/bench_parquet_parallel.py
@@ -7,15 +7,12 @@ from datetime import datetime
 file_path = "2013-07-train-00000-of-00001.parquet"
 
 
+pf = pq.ParquetFile(file_path)
+pylist = pf.read(columns=["movetext"]).column("movetext").to_pylist()
+
 a = datetime.now()
 
-pf = pq.ParquetFile(file_path)
-
-for i in range(pf.num_row_groups):
-    table = pf.read_row_group(0, columns=["movetext"])
-    extractors = rust_pgn_reader_python_binding.parse_games(
-        table.column("movetext").to_pylist()
-    )
+extractors = rust_pgn_reader_python_binding.parse_games(pylist)
 
 b = datetime.now()
 print(b - a)
