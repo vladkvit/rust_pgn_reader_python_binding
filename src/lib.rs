cde66503534299169f07f9d76a15a6eb423a681d
vladkvit <vladkvit@outlook.com>
2025-07-13 20:09:16 -0400

    Add legal moves per position

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,9 @@ pub struct MoveExtractor {
     moves: Vec<PyUciMove>,
 
     #[pyo3(get)]
+    legal_moves: Vec<Vec<PyUciMove>>,
+
+    #[pyo3(get)]
     valid_moves: bool,
 
     #[pyo3(get)]
@@ -144,6 +147,7 @@ impl MoveExtractor {
     fn new() -> MoveExtractor {
         MoveExtractor {
             moves: Vec::with_capacity(100),
+            legal_moves: Vec::with_capacity(100),
             pos: Chess::default(),
             valid_moves: true,
             comments: Vec::with_capacity(100),
@@ -219,6 +223,7 @@ impl Visitor for MoveExtractor {
     fn begin_movetext(&mut self, tags: Self::Tags) -> ControlFlow<Self::Output, Self::Movetext> {
         self.headers = tags;
         self.moves.clear();
+        self.legal_moves.clear();
         self.pos = Chess::default();
         self.valid_moves = true;
         self.comments.clear();
@@ -235,6 +240,35 @@ impl Visitor for MoveExtractor {
         san_plus: SanPlus,
     ) -> ControlFlow<Self::Output> {
         if self.valid_moves {
+            let legal_moves_for_pos = self.pos.legal_moves();
+            let mut uci_moves_for_pos: Vec<PyUciMove> =
+                Vec::with_capacity(legal_moves_for_pos.len());
+
+            for m in legal_moves_for_pos {
+                let uci_move_obj = UciMove::from_standard(&m);
+                match uci_move_obj {
+                    UciMove::Normal {
+                        from,
+                        to,
+                        promotion: promo_opt,
+                    } => {
+                        let py_uci_move = PyUciMove {
+                            from_square: from as u8,
+                            to_square: to as u8,
+                            promotion: promo_opt.map(|p_role| p_role as u8),
+                        };
+                        uci_moves_for_pos.push(py_uci_move);
+                    }
+                    _ => {
+                        eprintln!(
+                            "Unexpected UCI move type from standard PGN move: {:?}. Game moves might be invalid.",
+                            uci_move_obj
+                        );
+                    }
+                }
+            }
+            self.legal_moves.push(uci_moves_for_pos);
+
             match san_plus.san.to_move(&self.pos) {
                 Ok(m) => {
                     self.pos.play_unchecked(m);
