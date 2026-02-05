c6ed5a0fb0eac80f4dee07f587dbfa22fec4bc0b
vladkvit <vladkvit@outlook.com>
2026-02-04 21:45:01 -0500

    Delete unnecessary file

--- python/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Python wrapper utilities for rust_pgn_reader_python_binding."""
-
-from .wrapper import add_ergonomic_methods, GameView, BatchSlice
-
-__all__ = ["add_ergonomic_methods", "GameView", "BatchSlice"]
